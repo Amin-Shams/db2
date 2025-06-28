4998075f720abe90567059c3437558eb7f1804f8
Ali Shanazari <108313537+ALi12321SH@users.noreply.github.com>
2025-06-28 17:56:59 +0330

    Update 6 - Test RowCount SA-PortOperations

--- project/6 - Test RowCount SA.sql
+++ project/6 - Test RowCount SA.sql
@@ -22,3 +22,29 @@ ORDER BY
 
 
 SELECT * FROM Finance.ETLLog
+
+
+USE StagingDB;
+GO
+
+DECLARE @SchemaName NVARCHAR(128) = 'PortOperations';
+
+SELECT 
+    t.name AS TableName,
+    SUM(p.rows) AS [RowCount]  
+FROM 
+    sys.tables AS t
+INNER JOIN 
+    sys.schemas AS s ON t.schema_id = s.schema_id
+INNER JOIN 
+    sys.partitions AS p ON t.object_id = p.object_id
+WHERE 
+    p.index_id IN (0, 1) 
+    AND s.name = @SchemaName
+GROUP BY 
+    t.name
+ORDER BY 
+    [RowCount] DESC; 
+
+
+SELECT * FROM PortOperations.ETLLog
