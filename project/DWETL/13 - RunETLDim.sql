a6059e8814020a33766a31fa73986ee9b92c5dcc
Ali Shanazari <108313537+ALi12321SH@users.noreply.github.com>
2025-06-28 17:38:54 +0330

    Update 13 - RunETLDim-PortOperations

--- project/DWETL/13 - RunETLDim.sql
+++ project/DWETL/13 - RunETLDim.sql
@@ -161,3 +161,121 @@ END;
 GO
 
 exec Dim.UpdateAllDimensions;
+
+
+USE DataWarehouse;
+GO
+
+CREATE OR ALTER PROCEDURE Dim.ExecuteAllPortOpsDimensionUpdates
+AS
+BEGIN
+    SET NOCOUNT, XACT_ABORT ON;
+    DECLARE
+        @TableName NVARCHAR(128),
+        @StepStart DATETIME,
+        @StepEnd   DATETIME,
+        @Message   NVARCHAR(2000);
+
+    BEGIN TRY
+        BEGIN TRAN;
+
+        ------------------------------------------------------------------------
+        -- 1) DimDate
+        ------------------------------------------------------------------------
+        -- SET @TableName = 'Dim.DimDate';
+        -- SET @StepStart = GETDATE();
+        -- EXEC Dim.UpdateDimDateIncremental;
+        -- SET @StepEnd = GETDATE();
+        -- INSERT INTO dbo.ETLLog (TableName, OperationType, StartTime, EndTime, Message)
+        -- VALUES
+        --   (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimDateIncremental');
+
+        ------------------------------------------------------------------------
+        -- 2) DimShip (SCD2)
+        ------------------------------------------------------------------------
+        SET @TableName = 'Dim.DimShip';
+        SET @StepStart = GETDATE();
+        EXEC Dim.UpdateDimShipIncremental;
+        SET @StepEnd = GETDATE();
+        INSERT INTO dbo.ETLLog VALUES
+          (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimShipIncremental');
+
+        ------------------------------------------------------------------------
+        -- 3) DimPort (SCD1)
+        ------------------------------------------------------------------------
+        SET @TableName = 'Dim.DimPort';
+        SET @StepStart = GETDATE();
+        EXEC Dim.UpdateDimPortIncremental;
+        SET @StepEnd = GETDATE();
+        INSERT INTO dbo.ETLLog VALUES
+          (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimPortIncremental');
+
+        ------------------------------------------------------------------------
+        -- 4) DimContainer (SCD3)
+        ------------------------------------------------------------------------
+        SET @TableName = 'Dim.DimContainer';
+        SET @StepStart = GETDATE();
+        EXEC Dim.UpdateDimContainerIncremental;
+        SET @StepEnd = GETDATE();
+        INSERT INTO dbo.ETLLog VALUES
+          (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimContainerIncremental');
+
+        ------------------------------------------------------------------------
+        -- 5) DimEquipment (SCD1)
+        ------------------------------------------------------------------------
+        SET @TableName = 'Dim.DimEquipment';
+        SET @StepStart = GETDATE();
+        EXEC Dim.UpdateDimEquipmentIncremental;
+        SET @StepEnd = GETDATE();
+        INSERT INTO dbo.ETLLog VALUES
+          (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimEquipmentIncremental');
+
+        ------------------------------------------------------------------------
+        -- 6) DimEmployee (SCD2)
+        ------------------------------------------------------------------------
+        SET @TableName = 'Dim.DimEmployee';
+        SET @StepStart = GETDATE();
+        EXEC Dim.UpdateDimEmployeeIncremental;
+        SET @StepEnd = GETDATE();
+        INSERT INTO dbo.ETLLog VALUES
+          (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimEmployeeIncremental');
+
+        ------------------------------------------------------------------------
+        -- 7) DimYardSlot (SCD1)
+        ------------------------------------------------------------------------
+        SET @TableName = 'Dim.DimYardSlot';
+        SET @StepStart = GETDATE();
+        EXEC Dim.UpdateDimYardSlotIncremental;
+        SET @StepEnd = GETDATE();
+        INSERT INTO dbo.ETLLog VALUES
+          (@TableName, 'Update', @StepStart, @StepEnd, 'Executed UpdateDimYardSlotIncremental');
+
+        COMMIT;
+    END TRY
+    BEGIN CATCH
+        IF XACT_STATE() <> 0 ROLLBACK;
+
+        SET @StepEnd = GETDATE();
+        SET @Message = ERROR_MESSAGE();
+        INSERT INTO dbo.ETLLog (TableName, OperationType, StartTime, EndTime, Message)
+        VALUES
+          ('ALL_PORTOPS_DIMS', 'Error', @StepStart, @StepEnd, CONCAT('ExecuteAllPortOpsDimensionUpdates failed: ', @Message));
+
+        THROW;
+    END CATCH;
+END;
+GO
+
+-- Execute wrapper
+EXEC Dim.ExecuteAllPortOpsDimensionUpdates;
+GO
+
+-- Verify updated dimensions
+-- SELECT * FROM Dim.DimDate ORDER BY FullDate DESC;
+SELECT * FROM Dim.DimShip WHERE IsCurrent = 1;
+SELECT * FROM Dim.DimPort;
+SELECT * FROM Dim.DimContainer;
+SELECT * FROM Dim.DimEquipment;
+SELECT * FROM Dim.DimEmployee WHERE IsCurrent = 1;
+SELECT * FROM Dim.DimYardSlot;
+GO
